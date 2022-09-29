8a76446a448abd7b69a0539764b4a0648faf9ec7
Gideonchandar <114631460+Gideonchandar@users.noreply.github.com>
2022-09-29 11:36:09 +0700

    Add files via upload

---
+++ aplikasiBiodata.py.py
@@ -0,0 +1,20 @@
+# Aplikasi bidata buat saya gideon chandra duianto
+
+# masukan  nama
+# input data diri
+nama= input ("masukin nama anda!")\
+
+# print nama
+print("nama anda adalah: ",nama)
+
+# input kelas
+kelas= input ("masukin kelas anda!")\
+
+# print kelas
+print("kelas anda adalah: ",kelas)
+
+# input prodi
+prodi= input ("masukin prodi anda!")\
+
+# print prodi
+print("prodi anad adalah; ",prodi)
